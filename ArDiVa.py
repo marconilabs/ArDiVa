e7c9a49d7d4f68559c76292efb8f665692e720dd
Antonio Vaccarino <silverdrake@gmail.com>
2012-05-03 16:55:52 +0200

    Added copyright notice/license

--- ArDiVa.py
+++ ArDiVa.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import types
+
+# Copyright (C) 2012 Laboratori Guglielmo Marconi S.p.A. <http://www.labs.it>
+#
+# This file is part of SANET
+# SANET is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License
+#
+# SANET is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with SANET. If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'Antonio Vaccarino'
 __docformat__ = 'restructuredtext en'
 
+
+import types
 import re
 #very dirty hack so we can parse regular expressions as keysets
 RE_TYPE = type(re.compile(""))
